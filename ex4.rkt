1355550411e3af6d291b52af36875b142f7be076
Shlomi Alon <shlomi.al90@gmail.com>
2018-07-01 11:59:04 +0300

    Update ex4.rkt

--- ex4.rkt
+++ ex4.rkt
@@ -1,6 +1,9 @@
 #lang pl 03
-
-#| Shlomi Alon - 311610547 |#
+ 
+#|
+Elad Keyshawn - 315631887
+Shlomi Alon - 311610547
+|#
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -13,24 +16,22 @@
 ;==========BNF for ROL==========
 #| 
  <ROL> ::= {reg-len = <num> <RegE>}
-
  <RegE> ::=
             <Bits>                      |
             {and <RegE> <RegE>}         |
-            {or <RegE> <RegE>}          |
+            {or <RegE> <RegE>}
+            {xor <RegE> <RegE>}         |
             {shl <RegE>}                |
             {with {<ID> <RegE>} <RegE>} |
             {<ID>}                      |
             {call <RegE> <RegE>}        | 
             {fun <ID> <RegE>}           |
             {if <Bool> <RegE> <RegE>}
-
  <Bool> ::=
             false                       |
             true                        |
             {geq? <RegE> <RegE>}        |
             {maj? <RegE>}
-
  <Bits> ::=
             1                           |
             0                           |
@@ -48,6 +49,7 @@
  [Reg Bit-List]
  [And RegE RegE]
  [Or RegE RegE]
+ [Xor RegE RegE]
  [Shl RegE]
  [Id Symbol]
  [With Symbol RegE RegE]
@@ -55,8 +57,10 @@
  [Geq RegE RegE]
  [Maj RegE]
  [If RegE RegE RegE]
+ [Fun Symbol RegE]
  [Call RegE RegE]
- [Fun Symbol RegE])
+  ) 
+
 
 
 ;; Next is a technical function that converts (casts)
@@ -79,6 +83,7 @@
            (error 'parse-sexpr "wrong number of bits in ~s" a))]
     [(list 'and list1 list2) (And (parse-sexpr-RegL list1 reg-len) (parse-sexpr-RegL list2 reg-len))]
     [(list 'or list1 list2) (Or (parse-sexpr-RegL list1 reg-len) (parse-sexpr-RegL list2 reg-len))]
+    [(list 'xor list1 list2) (Xor (parse-sexpr-RegL list1 reg-len) (parse-sexpr-RegL list2 reg-len))]
     [(list 'shl list1) (Shl (parse-sexpr-RegL list1 reg-len))]
     [(list 'if list1 list2 list3)
      (If (parse-sexpr-RegL list1 reg-len) (parse-sexpr-RegL list2 reg-len) (parse-sexpr-RegL list3 reg-len))]
@@ -92,7 +97,15 @@
     [else (error 'parse-sexpr-RegE "bad with syntax in ~s" sexpr)])]
     [(list 'geq? list1 list2) (Geq (parse-sexpr-RegL list1 reg-len) (parse-sexpr-RegL list2 reg-len))]
     [(list 'maj? list1) (Maj(parse-sexpr-RegL list1 reg-len))]
-    [else (error 'parse-sexpr "bad syntax in ~s" sexpr)]))
+    [(cons 'fun more)
+       (match sexpr
+         [(list 'fun (list (symbol: name)) body)
+          (Fun name (parse-sexpr-RegL body reg-len))]
+         [else (error 'parse-sexpr-RegL "bad `fun' syntax in ~s" sexpr)])]
+    [(list 'call fun arg) (Call (parse-sexpr-RegL fun reg-len) (parse-sexpr-RegL arg reg-len))] ; Call
+    [else (error 'parse-sexpr-RegL "bad syntax in ~s" sexpr)]
+
+    )) 
 
 
 ;; to convert the main s-expression into ROL
@@ -123,6 +136,7 @@
 (define-type RES 
   [RegV Bit-List]
   [boolVal Boolean]
+  [FunV Symbol RegE ENV]
   )
 
 
@@ -145,6 +159,7 @@
     [(Reg n) expr]
     [(And l r) (And (subst l from to) (subst r from to))]
     [(Or l r) (Or (subst l from to) (subst r from to))]
+    [(Xor l r) (Xor (subst l from to) (subst r from to))]
     [(Geq l r) (Geq (subst l from to) (subst r from to))]
     [(Shl l) (Shl (subst l from to))]
     [(Id name) (if (eq? name from) to expr)]
@@ -181,8 +196,7 @@
 
 
 ;; Defining functions for dealing with arithmetic operations
-;; on the above types
- (: bit-and : BIT BIT -> BIT)
+(: bit-and : BIT BIT -> BIT)
 ;; Arithmetic and on two bits
  (define (bit-and a b)
    (cond
@@ -192,6 +206,16 @@
 (test (bit-and 0 1) => 0)
 (test (bit-and 1 1) => 1)
 
+(: bit-xor : BIT BIT -> BIT)
+;; Arithmetic and on two bits
+ (define (bit-xor a b)
+   (cond
+     [(eq? a b) 0]
+     [else 1]
+     ))
+;==========Test: bit-and==========
+(test (bit-xor 0 1) => 1)
+(test (bit-xor 1 1) => 0)
 
  (: bit-or : BIT BIT -> BIT)
 ;; Arithmetic and on two bits
@@ -279,47 +303,112 @@
      [else (error 'RegV->bit-list "error in RegV->bit-list")]))
 
 
-(: eval : RegE -> RES)
+ ;; Types for environments, values, and a lookup function
+
+  (define-type ENV
+    [EmptyEnv]
+    [Extend Symbol RES ENV])
+
+  (: lookup : Symbol ENV -> RES)
+  (define (lookup name env)
+    (cases env
+      [(EmptyEnv) (error 'lookup "no binding for ~s" name)]
+      [(Extend id val rest-env)
+       (if (eq? id name) val (lookup name rest-env))]))
+
+(: lookup2 : Symbol ENV -> Boolean)
+  (define (lookup2 name env)
+    (cases env
+      [(EmptyEnv) #t]
+      [(Extend id val rest-env)
+       (if (eq? id name) #f (lookup2 name rest-env))]))
+
+
+;; Runs through the AST tree recursively if at anypoint the lookup table won't find variable in env, it'll return true,
+;; and since all recursive calls are connected with "or" the true value will bubble up the stack so the func will return true.
+(: containsFreeInstance? : RegE ENV -> Boolean)
+(define (containsFreeInstance? expr env)
+  (cases expr
+    [(Reg b)  #f]
+    [(And l r) ( or (containsFreeInstance? l env) (containsFreeInstance? r env))]
+    [(Or l r) (or (containsFreeInstance? l env) (containsFreeInstance? r env))]
+    [(Xor l r) (or (containsFreeInstance? l env) (containsFreeInstance? r env))]
+    [(Shl l) (containsFreeInstance? l env)]
+    [(Geq reg1 reg2) (or (containsFreeInstance? reg1 env) (containsFreeInstance? reg2 env))]
+    [(Maj l) (containsFreeInstance? l env)]
+    [(If condition if_T if_F)(or (containsFreeInstance? condition env) (containsFreeInstance? if_T env) (containsFreeInstance? if_F env))]
+    [(Id sym) (if (or (eq? sym 'false) (eq? sym 'true)) #f (lookup2 sym env))]
+    [(Bool l) #f]
+    [(With bound-id named-expr bound-body)
+       (containsFreeInstance? bound-body
+             (Extend bound-id (boolVal (containsFreeInstance? named-expr env)) env))]
+    [(Fun bound-id bound-body)
+       (containsFreeInstance? bound-body 
+             (Extend bound-id (boolVal(containsFreeInstance? bound-body env)) env))]
+      [(Call fun-expr arg-expr) (if (or (containsFreeInstance? fun-expr env) (containsFreeInstance? fun-expr env)) #t #f)]
+     )
+  )
+
+(: check-code : String -> Boolean) (define (check-code str)
+(containsFreeInstance? (parse str) (EmptyEnv)))
+
+(test (check-code "{ reg-len = 3 {call {fun {x} {or x {1 0 1}}} {0 0 1}}}") => #f)
+(test (check-code "{ reg-len = 3 {call {fun {y} {xor x {0 0 1}}} {0 0 1}}}") => #t)
+(test (check-code "{ reg-len = 3 {call foo {0 0 1}}}") => #t)
+(test (check-code "{ reg-len = 3 {fun {x} {and x {or {1 0 1} {shl {0 0 0}}}}}}") => #f)
+(test (check-code "{ reg-len = 4 {with {x {1 1 1 1}} {shl y}}}") => #t)
+
+(: eval : RegE ENV -> RES)
 ;; evaluates RegE expressions by reducing them to bit-lists
 ;; return bit-list or boolean. 
-(define (eval expr)
+(define (eval expr env)
    (cases expr
     [(Reg b)  (RegV b)]
-    [(And l r) (reg-arith-op bit-and (eval l) (eval r))]
-    [(Or l r) (reg-arith-op bit-or (eval l) (eval r))]
-    [(Shl l) (RegV (shift-left (RegV->bit-list (eval l))))]
-    [(Geq reg1 reg2) (boolVal (geq-bitlists? (RegV->bit-list (eval reg1)) (RegV->bit-list (eval reg2))))]
-    [(Maj l) (boolVal(majority? (RegV->bit-list(eval l))))]
-    [(If condition if_T if_F)(cases (eval condition)[(boolVal boo) (if (eq? boo #t) (eval if_T) (eval if_F))][else (error 'eval "error in ~s" condition)])]
-    [(Id sym) (if (eq? sym 'false)(boolVal #f)(if (eq? sym 'true)(boolVal #t)(error 'eval "free identifier: ~s" sym)))]
-    [(Bool l) (boolVal l)]
-    [(With sym reg1 reg2) (eval (subst reg2 sym reg1))]
-    [(Fun bound-id bound-body) expr]
+    [(And l r) (reg-arith-op bit-and (eval l env) (eval r env))]
+    [(Or l r) (reg-arith-op bit-or (eval l env) (eval r env))]
+    [(Xor l r) (reg-arith-op bit-xor (eval l env) (eval r env))]
+    [(Shl l) (RegV (shift-left (RegV->bit-list (eval l env))))]
+    [(Geq reg1 reg2) (boolVal (geq-bitlists? (RegV->bit-list (eval reg1 env)) (RegV->bit-list (eval reg2 env))))]
+    [(Maj l) (boolVal(majority? (RegV->bit-list(eval l env))))]
+    [(If condition if_T if_F)(cases (eval condition env)[(boolVal boo) (if (eq? boo #t) (eval if_T env) (eval if_F env))][else (error 'eval "error in ~s" condition)])]
+    [(Id sym) (if (eq? sym 'false)(boolVal #f)(if (eq? sym 'true)(boolVal #t)(lookup sym env)))]
+
+     [(Bool l) (boolVal l)]
+    [(With bound-id named-expr bound-body)
+       (eval bound-body
+             (Extend bound-id (eval named-expr env) env))]
+    [(Fun bound-id bound-body)
+       (FunV bound-id bound-body env)]
     [(Call fun-expr arg-expr)
-     (let ([fval (eval fun-expr)])
+       (let ([fval (eval fun-expr env)])
          (cases fval
-          [(Fun bound-id bound-body) (eval (subst bound-body bound-id (eval arg-expr)))]
-          [else (error 'eval "`call' expects a function, got: ~s" fval)]
-           ))]
+           [(FunV bound-id bound-body f-env)
+            (eval bound-body
+                  (Extend bound-id (eval arg-expr env) f-env))]
+           [else (error 'eval "`call' expects a function, got: ~s"
+                              fval)]))]
      ))
-
-
+ 
+ 
 ;==========Test: eval(operator)==========
-(test (eval (Reg '(1 0 1 1))) => (RegV '(1 0 1 1)))
-(test (eval (And (Reg '(1 1 0 0)) (Reg '(1 0 1 0)))) => (RegV '(1 0 0 0)))
-(test (eval (Or (Reg '(1 1 0 0)) (Reg '(1 0 1 0)))) => (RegV '(1 1 1 0)))
-(test (eval (Shl (Reg '(0 0 0 1)))) => (RegV '(0 0 1 0)))
-(test (eval (Id 'a)) =error> "free identifier: a")
-(test (eval (Id 'false)))
-(test (eval (Id 'true)))
-(test (eval (With 'x (Or (Reg '(1 0 1 0)) (Reg '(0 1 0 0))) (Shl (Shl (Shl (Id 'x)))))) => (RegV '(0 1 1 1)))
-(test (eval (Bool #f)) => (boolVal #f))
-(test (eval (Geq (Reg '(0 0 1 1)) (Reg '(0 1 1 1)))) => (boolVal #f))
-(test (eval (Maj (Reg '(1 0 0 0)))) => (boolVal #f))
+(test (eval (Reg '(1 0 1 1)) (EmptyEnv)) => (RegV '(1 0 1 1)))
+(test (eval (And (Reg '(1 1 0 0)) (Reg '(1 0 1 0))) (EmptyEnv)) => (RegV '(1 0 0 0)) )
+(test (eval (Or (Reg '(1 1 0 0)) (Reg '(1 0 1 0))) (EmptyEnv)) => (RegV '(1 1 1 0)) )
+(test (eval (Xor (Reg '(1 1 0 0)) (Reg '(1 0 1 0))) (EmptyEnv)) => (RegV '(0 1 1 0)) )
+(test (eval (Shl (Reg '(0 0 0 1))) (EmptyEnv)) => (RegV '(0 0 1 0)))
+;(test (eval (Id 'a) (EmptyEnv)) =error> "free identifier: a")
+(test (eval (Id 'false) (EmptyEnv)))
+(test (eval (Id 'true) (EmptyEnv)))
+(test (eval (With 'x (Or (Reg '(1 0 1 0)) (Reg '(0 1 0 0))) (Shl (Shl (Shl (Id 'x))))) (EmptyEnv)) => (RegV '(0 1 1 1)))
+(test (eval (Bool #f) (EmptyEnv)) => (boolVal #f))
+(test (eval (Geq (Reg '(0 0 1 1)) (Reg '(0 1 1 1))) (EmptyEnv)) => (boolVal #f))
+(test (eval (Maj (Reg '(1 0 0 0))) (EmptyEnv)) => (boolVal #f))
 (test (eval (If (Bool #t)
                 (And (Reg '(1 1 0 0)) (Reg '(1 0 1 0)))
-                (Or (Reg '(1 1 0 0)) (Reg '(1 0 1 0))))) => (RegV '(1 0 0 0)))
+                (Or (Reg '(1 1 0 0)) (Reg '(1 0 1 0)))) (EmptyEnv)) => (RegV '(1 0 0 0)))
 
+;; New Fun and Call tests
+;(test (eval (Call (Fun 'x (Maj x)) (Reg '(1 0 0 0))) (EmptyEnv)) => (boolVal #f) )
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -332,7 +421,7 @@
 ;; evaluate a ROL program contained in a string
 ;; we will not allow to return a boolean type
 (define (run str)
-  (cases (eval (parse str))
+  (cases (eval (parse str) (EmptyEnv))
     [(RegV bl) bl]
     [else (error 'run "error- can't return boolean!")]))
 
@@ -345,7 +434,9 @@
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-;;More general tests 
+
+;;More general tests
+(test (run "{ reg-len =  3  {call {fun {x} {and x x}} {1 1 1} }}") => '(1 1 1))
 (test (run "{ reg-len =  4  {1 0 0 0}}") => '(1 0 0 0))  
 (test (run "{ reg-len = 4  {shl {1 0 0 0}}}") => '(0 0 0 1)) 
 (test (run "{ reg-len = 4 {and {shl {1 0 1 0}}{shl {1 0 1 0}}}}") => '(0 1 0 1)) 
@@ -355,7 +446,7 @@
 (test (run "{ reg-len = 3 {with {x {0 0 0}} {if {geq? {1 0 1} x} {0 0 1} {1 1 0}}}}") => '(0 0 1))
 (test (run "{ reg-len = 3  {if {geq? {1 0 1} {1 1 1}} true false}}") =error> "error- can't return boolean!" )
 (test (RegV->bit-list (RegV '(1 0 0 0))) => '(1 0 0 0))
-(test (run "{ reg-len = 4 {with {x {1 1 1 1}} {shl y}}}") =error> "free identifier: y") 
+;(test (run "{ reg-len = 4 {with {x {1 1 1 1}} {shl y}}}") =error> "free identifier: y") 
 (test (run "{ reg-len = 2 { with {x { or {and {shl {1 0}} {1 0}} {1 0}}} {shl x}}}") => '(0 1)) 
 (test (run "{ reg-len = 4 {or {1 1 1 1} {0 1 1}}}") =error> "wrong number of bits in (0 1 1)") 
 (test (run "{ reg-len =  0  {}}") =error> "Register length must be at least 1")
@@ -380,5 +471,33 @@
 (test (subst (Bool #t) 'x (Bool #t)) => (Bool #t))
 (test (run "{ reg-len = 4 {with {y {0 1 1 0 }} {with {x {1 1 1 1}} {shl x}}}}") => '(1 1 1 1))
 (test (run "{ reg-len = 4 {with {x {1 1 1 1}} {with {y {1 0 1 0}} {and x y}}}}") => '(1 0 1 0))
-(test (run "{ reg-len = 3 {if {geq? {1 0 1} {1 1 1}} {0 0 1} {1 1 0}}}") => '(1 1 0)) 
+(test (run "{ reg-len = 3 {if {geq? {1 0 1} {1 1 1}} {0 0 1} {1 1 0}}}") => '(1 1 0))
+(test (run "{ reg-len = 3 {if {geq? {1 0 1} {1 1 1}} {0 0 1} {1 1 0}}}") => '(1 1 0))
 
+(test (run "{ reg-len = 4 {1 0 0 0}}") => '(1 0 0 0))
+ (test (run "{ reg-len = 4 {xor {1 0 0 0} {1 1 1 1}}}") =>
+'(0 1 1 1))
+ (test (run "{ reg-len = 4 {shl {1 0 0 0}}}") => '(0 0 0 1))
+ (test (run "{ reg-len = 4
+ {and {shl {1 0 1 0}}{shl {1 0 1 0}}}}") =>
+'(0 1 0 1))
+ (test (run "{ reg-len = 4
+ { or {and {shl {1 0 1 0}}
+ {shl {1 0 0 1}}} {1 0 1 0}}}") =>
+'(1 0 1 1))
+ (test (run "{ reg-len = 2
+ { or {and {shl {1 0}} {1 0}} {1 0}}}") => '(1
+0))
+ (test (run "{ reg-len = 4
+ {with {x {1 1 1 1}} {shl y}}}") =error>
+"lookup: no binding for y")
+
+#| Dont forget to add these error handlings
+(test (run "{ reg-len = 4
+ {or {1 1 1 1} {0 1 1}}}") =error>
+ "wrong number of bits
+in (0 1 1)")
+ (test (run "{ reg-len = 0 {}}") =error>
+ "Register length must be
+at least 1")
+|#
